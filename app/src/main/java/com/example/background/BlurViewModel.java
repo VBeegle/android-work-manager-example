51085efb636e2893f9a67a6992ade6d04bff6351
Victoria <tazzygirl37@gmail.com>
2020-02-18 17:54:01 -0800

    Adds completed BlurWorker and BlurViewModel

--- app/src/main/java/com/example/background/BlurViewModel.java
+++ app/src/main/java/com/example/background/BlurViewModel.java
@@ -18,24 +18,40 @@ package com.example.background;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
 import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.example.background.workers.BlurWorker;
+
+import static com.example.background.Constants.KEY_IMAGE_URI;
+
 public class BlurViewModel extends AndroidViewModel {
 
     private Uri mImageUri;
+    private WorkManager mWorkManager;
 
     public BlurViewModel(@NonNull Application application) {
+
         super(application);
+        mWorkManager = WorkManager.getInstance(application);
     }
+
 
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
     void applyBlur(int blurLevel) {
-
+        OneTimeWorkRequest blurRequest =
+                new OneTimeWorkRequest.Builder(BlurWorker.class)
+                .setInputData(createInputDataForUri())
+                .build();
+        mWorkManager.enqueue(blurRequest);
     }
 
     private Uri uriOrNull(String uriString) {
@@ -43,6 +59,15 @@ public class BlurViewModel extends AndroidViewModel {
             return Uri.parse(uriString);
         }
         return null;
+    }
+
+
+    private Data createInputDataForUri(){
+        Data.Builder builder = new Data.Builder();
+        if(mImageUri != null){
+            builder.putString(KEY_IMAGE_URI, mImageUri.toString());
+        }
+        return builder.build();
     }
 
     /**
